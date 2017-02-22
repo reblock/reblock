3c53dea20958589edc6d7a418fc71ee1113e5035
sainthkh <sainthkh@naver.com>
2017-02-22 10:24:32 +0900

    Removed unnecessary files.

--- web/frontend/app/pages/common/auth/UIPackage.tsx
+++ web/frontend/app/pages/common/auth/UIPackage.tsx
@@ -2,7 +2,6 @@
 import * as React from 'react'
 import { SignupDialog } from './SignupDialog'
 import { LoginDialog } from './LoginDialog'
-import { SubscribeDialog } from './SubscribeDialog'
 
 export default class AuthUI extends React.Component<{}, {}> {
 	render() {
@@ -10,7 +9,6 @@ export default class AuthUI extends React.Component<{}, {}> {
 			<div>
 				<SignupDialog />
 				<LoginDialog />
-				<SubscribeDialog />
 			</div>
 		);
 	}
