50d9279c7dbff29b6e97d62902ed404e66b0ce4b
sainthkh <sainthkh@naver.com>
2017-02-24 15:16:14 +0900

    Fixed Spinner style: not in the center, added vertical margin.

--- web/frontend/components/Spinner.tsx
+++ web/frontend/components/Spinner.tsx
@@ -3,8 +3,8 @@ import RefreshIndicator from 'material-ui/RefreshIndicator'
 
 export const Spinner = props => {
 	return (
-		<div style={{margin:"0 auto", height: `${props.size}px`}}>
-			<div style={{position: "relative", width: `${props.size}px`}}>
+		<div style={{height: `${props.size}px`}}>
+			<div style={{position: "relative", margin:`${props.marginVertical || 20}px auto`, width: `${props.size}px`}}>
 				<RefreshIndicator left={0} top={0} {...props} />
 			</div>
 		</div>
