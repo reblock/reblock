f22e0928f6b09ba984e46f0e5905ea1a9dc06224
sainthkh <sainthkh@naver.com>
2017-02-24 10:24:49 +0900

    Added design properties for LoginForm.

--- web/frontend/components/LoginForm.tsx
+++ web/frontend/components/LoginForm.tsx
@@ -1,18 +1,44 @@
 import * as React from 'react'
 
+import { Styler } from './Styler'
 import { Form } from './Form'
+import { CheckBox } from './CheckBox'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
 
-export const LoginForm = ({ title, admin, onResponse }) => {
+interface LoginFormProps {
+	admin: boolean
+	onResponse?: (any) => any
+	showRememberMe?: boolean
+	rememberUser?: boolean
+	
+	textFieldStyle?: any
+	rememberMeStyle?: any
+	buttonStyle?: any
+}
+
+export const LoginForm = Styler((props: LoginFormProps) => {
+	props = Object.assign({
+		admin: false,
+		onResponse: a => (a),
+		showRememberMe: false,
+		rememberUser: true,
+		textFieldStyle: {},
+		rememberMeStyle: {},
+		buttonStyle: {},
+	}, props)
+	var { admin, onResponse, showRememberMe, rememberUser, 
+		textFieldStyle, rememberMeStyle, buttonStyle } = props
+	
 	return (
 		<Form name="login-form" admin={admin} resource="/auth/login" style={form} onResponse={onResponse}>
-			<TextField name="id" hintText="ID" style={width100} />
-			<TextField name="password" type="password" hintText="Password" style={width100} />
-			<RaisedButton type="submit" label="Log in" primary={true} style={width100}/>
+			<TextField name="id" hintText="ID" style={[textField, textFieldStyle]} />
+			<TextField name="password" type="password" hintText="Password" style={[textField, textFieldStyle]} />
+			<CheckBox name="rememberMe" style={[(showRememberMe ? {} : hidden), rememberMeStyle]} label="Remember me" defaultValue={rememberUser} />
+			<RaisedButton type="submit" label="Log in" primary={true} style={[button, buttonStyle]}/>
 		</Form>
 	)
-}
+})
 
 const form: React.CSSProperties = {
 	width: "280px",
@@ -21,6 +47,15 @@ const form: React.CSSProperties = {
 	justifyContent: "center",
 }
 
-const width100 = {
+const textField = {
 	width: "100%",
+}
+
+const hidden = {
+	display: "none",
+}
+
+const button = {
+	width: "100%",
+	marginTop: "12px",
 }
